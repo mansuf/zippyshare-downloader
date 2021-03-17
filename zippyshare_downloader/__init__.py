b5800cb04ff9f81ccb573d4b4e048ec7cd063aaf
Rahman Yusuf <danipart4@gmail.com>
2021-03-17 07:54:26 +0700

    fixed issue when getting download URL

--- zippyshare_downloader/__init__.py
+++ zippyshare_downloader/__init__.py
@@ -13,8 +13,10 @@ from zippyshare_downloader.utils import getStartandEndvalue
 from bs4 import BeautifulSoup
 from download import download as dl
 from .utils import check_valid_zippyshare_url
+from .errors import ParserError
 import requests
 import math
+import io
 
 class Zippyshare:
     ALLOWED_NAMES = {
@@ -34,13 +36,76 @@ class Zippyshare:
 
     def _get_url(self, u, r: requests.Request):
         self._logger_info('Getting Download URL')
-        startpos_init = r.text.find('document.getElementById(\'dlbutton\').href')
-        scrapped_init = r.text[startpos_init:]
-        endpos_init = scrapped_init.find('</script>')
+        # Getting download button javascript code
+        parser = BeautifulSoup(r.text, 'html.parser')
+        for script in parser.find_all('script'):
+            if 'document.getElementById(\'dlbutton\').href' in script.decode_contents():
+                scrapped_script = script.decode_contents()
+                break
+            else:
+                scrapped_script = None
+        if scrapped_script is None:
+            raise ParserError('download button javascript cannot be found')
+
+        # Finding omg attribute value in dlbutton element
+        elements = io.StringIO(scrapped_script).readlines()
+        omg_element = 'document.getElementById(\'dlbutton\').omg = '
+        for element in elements:
+            e = element.strip()
+            if e.startswith(omg_element):
+                omg = e.replace(omg_element, '').replace('"', '').replace(';', '')
+                break
+            else:
+                omg = None
+        if omg is None:
+            raise ParserError('omg attribute in download button javascript cannot be found')
+
+        # Finding uncompiled Random Number between FileID and Filename
+        # http://www.zippyshare.com/d/{FileID}/uncompiled_number/{Filename}
+        startpos_init = scrapped_script.find('document.getElementById(\'dlbutton\').href')
+        scrapped_init = scrapped_script[startpos_init:]
+        endpos_init = scrapped_init.find(';')
         scrapped = scrapped_init[:endpos_init]
-        element_value = scrapped[:scrapped.find(';')].replace('document.getElementById(\'dlbutton\').href = ', '')
+        element_value = scrapped.replace('document.getElementById(\'dlbutton\').href = ', '')
         url_download_init = getStartandEndvalue(element_value, '"')
-        random_number = getStartandEndvalue(element_value, '(', ')')
+        uncompiled_number = getStartandEndvalue(element_value, '(', ')').replace('%b', '')
+        
+        # Finding Random Number variable a in scrapped_script
+        variables = io.StringIO(scrapped_script).readlines()
+        for var in variables:
+            if var.strip().startswith('var a = '):
+                a = var.strip().replace('var a = ', '').replace(';', '')
+                break
+            else:
+                a = None
+        if a is None:
+            raise ParserError('variable a in download button javascript cannot be found')
+
+        # Finding Random Number variable b in scrapped_script
+        variables = io.StringIO(scrapped_script).readlines()
+        for var in variables:
+            if var.strip().startswith('var b = '):
+                b = var.strip().replace('var b = ', '').replace(';', '')
+                break
+            else:
+                b = None
+        if b is None:
+            raise ParserError('variable b in download button javascript cannot be found')
+
+        # Determine if variable a or b is used 
+        # then replace the name variable with value variable
+        # in uncompiled_number
+        if omg != 'f':
+            if 'a' in uncompiled_number:
+                random_number = uncompiled_number.replace('a', str(math.ceil(int(a)/3)))
+            else:
+                random_number = uncompiled_number.replace('b', str(math.ceil(int(b)/3)))
+        else:
+            if 'a' in uncompiled_number:
+                random_number = uncompiled_number.replace('a', str(math.floor(int(a)/3)))
+            else:
+                random_number = uncompiled_number.replace('a', str(math.floor(int(a)/3)))
+
         # Now using self.evaluate() to safely do math calculations
         url_number = str(self.evaluate(random_number))
         continuation_download_url_init = getStartandEndvalue(element_value, '(')
