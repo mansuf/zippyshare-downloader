b3f2b4a7c2158c8f7bcddffef759248f9f25b6f7
Rahman Yusuf <danipart4@gmail.com>
2021-04-08 18:58:29 +0700

    added folder and custom_filename to CLI

--- zippyshare_downloader/__main__.py
+++ zippyshare_downloader/__main__.py
@@ -11,18 +11,18 @@ def check_valid_zippyshare_url(url):
     else:
         return cvzu(url)
 
-def extract_multi_urls(z, urls, download=True, silent=True):
+def extract_multi_urls(z, urls, download=True, silent=True, folder=None):
     us = []
     result = {'urls': us}
     for u in urls:
-        us.append(z.extract_info(u, download=download))
+        us.append(z.extract_info(u, download=download, folder=folder))
     if not silent and download is True:
         print(json.dumps(result))
     elif download is False:
         print(json.dumps(result))
 
-def extract_single_urls(z, url, download=True, silent=True):
-    result = json.dumps(z.extract_info(url, download=download))
+def extract_single_urls(z, url, download=True, silent=True, folder=None, custom_filename=None):
+    result = json.dumps(z.extract_info(url, download=download, folder=folder, custom_filename=custom_filename))
     if not silent and download is True:
         print(result)
     elif download is False:
@@ -35,6 +35,8 @@ def main():
     parser.add_argument('--verbose', action='store_true', help='Enable verbose')
     parser.add_argument('--replace', action='store_true', help='Replace file if exist')
     parser.add_argument('--silent', action='store_true', help='No output')
+    parser.add_argument('--output-folder', help='Store downloaded file in given folder', metavar='OUTPUT_FOLDER')
+    parser.add_argument('--filename', help='Rewrite filename. will be ignored if using file containing zippyshare urls')
     args = parser.parse_args()
     urls = args.__dict__['ZIPPYSHARE_URL or FILE']
     args.progress_bar = True
@@ -48,14 +50,28 @@ def main():
     )
     if isinstance(urls, list):
         if args.no_download:            
-            extract_multi_urls(z, urls, download=False, silent=args.silent)
+            extract_multi_urls(z, urls, download=False, silent=args.silent, folder=args.output_folder)
         else:
-            extract_multi_urls(z, urls, download=True, silent=args.silent)
+            extract_multi_urls(z, urls, download=True, silent=args.silent, folder=args.output_folder)
     else:
         if args.no_download:
-            extract_single_urls(z, urls, download=False, silent=args.silent)
+            extract_single_urls(
+                z,
+                urls,
+                download=False,
+                silent=args.silent,
+                folder=args.output_folder,
+                custom_filename=args.filename
+            )
         else:
-            extract_single_urls(z, urls, download=True, silent=args.silent)
+            extract_single_urls(
+                z,
+                urls,
+                download=True,
+                silent=args.silent,
+                folder=args.output_folder,
+                custom_filename=args.filename
+            )
 
 if __name__ == "__main__":
     main()
