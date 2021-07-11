841a4c9986d75583d613de073742bf92cb835a08
Rahman Yusuf <danipart4@gmail.com>
2021-07-11 14:10:49 +0700

    added test for download urls

--- test-compatibility.py
+++ test-compatibility.py
@@ -1,7 +1,15 @@
-from zippyshare_downloader import Zippyshare
-
-z = Zippyshare(replace=True)
+import requests
+from zippyshare_downloader import extract_info
 
 url = 'https://www54.zippyshare.com/v/bbvLtnKG/file.html'
 
-print(z.extract_info(url, download=False))
+file = extract_info(url, download=False)
+download_url = file.download_url
+
+
+response = requests.get(download_url, stream=True)
+
+if response.url != download_url:
+    raise Exception('Download url redirected')
+
+response.close()
