b24217d779b18d2f411d0e9c986d0c913bff77fe
Rahman Yusuf <danipart4@gmail.com>
2021-07-11 13:50:15 +0700

    simplified regexs zippyshare urls

--- zippyshare_downloader/utils.py
+++ zippyshare_downloader/utils.py
@@ -25,23 +25,21 @@ def evaluate(expression):
 
 REGEXS_ZIPPYSHARE_URL = [
     # View zippyshare url
-    r'https:\/\/www[0-9]{3}\.zippyshare\.com\/v\/[0-9A-Za-z]{8}\/file\.html',
-    r'https:\/\/www[0-9]{2}\.zippyshare\.com\/v\/[0-9A-Za-z]{8}\/file\.html',
-    r'https:\/\/www[0-9]{1}\.zippyshare\.com\/v\/[0-9A-Za-z]{8}\/file\.html',
+    r'https:\/\/www[0-9]{1,3}\.zippyshare\.com\/v\/[0-9A-Za-z]{8}\/file\.html',
     r'https:\/\/www\.zippyshare\.com\/v\/[0-9A-Za-z]{8}\/file\.html',
     # Download Zippyshare url
-    r'^https:\/\/www[0-9]{3}\.zippyshare\.com\/d\/[0-9A-Za-z]{8}\/',
-    r'https:\/\/www[0-9]{2}\.zippyshare\.com\/d\/[0-9A-Za-z]{8}\/',
-    r'https:\/\/www[0-9]{1}\.zippyshare\.com\/d\/[0-9A-Za-z]{8}\/',
+    r'https:\/\/www[0-9]{1,3}\.zippyshare\.com\/d\/[0-9A-Za-z]{8}\/',
     r'https:\/\/www\.zippyshare\.com\/d\/[0-9A-Za-z]{8}\/',
 ]
 
 def check_valid_zippyshare_url(url):
+    """Check if given url is valid Zippyshare url"""
     for regex in REGEXS_ZIPPYSHARE_URL:
         if re.match(regex, url) is not None:
             return url
     raise InvalidURL('"%s" is not a zippyshare url' % (url))
 
+# TODO: Document this !
 def getStartandEndvalue(value: str, sub: str, second_sub=None):
     v = value[value.find(sub)+1:]
     if second_sub is not None:
