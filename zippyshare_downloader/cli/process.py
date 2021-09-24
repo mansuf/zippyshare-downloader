627bf98a9a296a5db08e4f0434eded0d606782a0
Rahman Yusuf <danipart4@gmail.com>
2021-09-25 01:23:17 +0700

    reworked and optimized CLI app

---
+++ zippyshare_downloader/cli/process.py
@@ -0,0 +1,119 @@
+import asyncio
+import json
+import logging
+from .utils import setup_args, setup_logging, build_kwargs
+from ..fetcher import extract_info_coro, download_coro, extract_info, download
+
+__all__ = (
+    'main',
+)
+
+def process(**kwargs):
+    # We don't do fast download in non-async here
+    fast = kwargs.pop('fast')
+
+    urls = kwargs.pop('urls')
+    
+    # If urls is grabbed from file
+    if isinstance(urls, list):
+
+        # If --no-download is specified
+        if not kwargs.get('download'):
+
+            # Warn the users if --unzip is specified
+            # while --no-download is exist
+            if kwargs.get('unzip'):
+                log.warning('--unzip is set, while --no-download is also set. Ignoring --unzip')
+
+            # Begin the fetching !!!
+            files = []
+            for url in urls:
+                files.append(extract_info(url, **kwargs))
+        
+        # If download is yes
+        else:
+            kwargs.pop('download')
+            files = download(*urls, **kwargs)
+
+            # Warn the users if --fast is specified
+            # and --async is not
+            if fast:
+                log.warning('--fast is set and --async is not. Ignoring --fast option')
+
+            # Warn the users if --filename is specified
+            # while using multiple zippyshare urls
+            if kwargs.get('filename'):
+                log.warning('Using multi zippyshare urls and --filename is set. Ignoring --filename option')
+        
+        # Print all files informations in JSON format
+        print(json.dumps({"urls": [file.to_dict() for file in files]}))
+
+    # If urls is single url
+    else:
+        # Warn the users if --fast is specified
+        # and --async is not
+        if fast:
+            log.warning('--fast is set and --async is not. Ignoring --fast option')
+
+        kwargs.pop('zip')
+        file = extract_info(urls, **kwargs)
+        print(file.to_JSON())
+
+async def process_async(**kwargs):
+    urls = kwargs.pop('urls')
+    
+    # If urls is grabbed from file
+    if isinstance(urls, list):
+
+        # If --no-download is specified
+        if not kwargs.get('download'):
+
+            # Warn the users if --unzip is specified
+            # while --no-download is exist
+            if kwargs.get('unzip'):
+                log.warning('--unzip is set and --no-download is also set. Ignoring --unzip option')
+
+            # Begin the fetching !!!
+            files = []
+            for url in urls:
+                files.append(await extract_info_coro(url, **kwargs))
+        
+        # If download is yes
+        else:
+            # Delete download parameter
+            kwargs.pop('download')
+            
+            # Warn the users if --filename is specified
+            # while using multiple zippyshare urls
+            if kwargs.get('filename'):
+                log.warning('Using multi zippyshare urls and --filename is set. Ignoring --filename option')
+
+            files = await download_coro(*urls, **kwargs)
+        
+        # Print all files informations in JSON format
+        print(json.dumps({"urls": [file.to_dict() for file in files]}))
+
+    # If urls is single url
+    else:
+        kwargs.pop('zip')
+        file = await extract_info_coro(urls, **kwargs)
+        print(file.to_JSON())
+
+def main():
+    global log
+    log = logging.getLogger('zippyshare_downloader_null_logging').addHandler(logging.NullHandler())
+
+    # Parse parameters
+    args = setup_args()
+
+    # Setup logging
+    if not args.silent:
+        log = setup_logging('zippyshare_downloader', args.verbose)
+    
+    kwargs = build_kwargs(args, args.urls)
+
+    async_process = kwargs.pop('async')
+    if not async_process:
+        process(**kwargs)
+    else:
+        asyncio.run(process_async(**kwargs))
---
+++ zippyshare_downloader/cli/utils.py
@@ -0,0 +1,132 @@
+import argparse
+import logging
+import os
+from ..utils import check_valid_zippyshare_url
+
+def _check_urls(url):
+    if os.path.exists(url):
+        with open(url, 'r') as opener:
+            return [check_valid_zippyshare_url(i) for i in opener.read().splitlines()]
+    else:
+        return check_valid_zippyshare_url(url)
+
+def setup_args():
+    parser = argparse.ArgumentParser(description='Download file from zippyshare directly from python')
+
+    # URL or File location
+    parser.add_argument(
+        'ZIPPYSHARE_URL or FILE',
+        type=_check_urls,
+        help='Zippyshare URL or file containing zippyshare urls',
+    )
+
+    # No download
+    parser.add_argument(
+        '--no-download',
+        action='store_true',
+        help='No download file'
+    )
+
+    # Verbose output
+    parser.add_argument(
+        '--verbose',
+        '-v',
+        action='store_true',
+        help='Enable verbose output'
+    )
+
+    # Replace downloaded file (if exist)
+    parser.add_argument(
+        '--replace',
+        '-r',
+        action='store_true',
+        help='Replace file if exist'
+    )
+
+    # No Output
+    parser.add_argument(
+        '--silent',
+        '-s',
+        action='store_true',
+        help='No output'
+    )
+
+    # Store folder
+    parser.add_argument(
+        '--folder',
+        help='Store downloaded file in given folder',
+        metavar='FOLDER'
+    )
+
+    # Change filename
+    parser.add_argument(
+        '--filename',
+        help='Set a replacement filename. will be ignored if using multiple zippyshare urls',
+        metavar='FILENAME'
+    )
+
+    # Zip
+    parser.add_argument(
+        '--zip',
+        '-z',
+        help='Zip all downloaded files (if using multiple zippyshare urls) once finished' \
+             'the zip filename will be taken from this option. NOTE: you can\'t combine' \
+             '--zip and --unzip options, it will throw error.',
+        metavar='FILENAME',
+    )
+
+    # Unzip each downloaded file
+    parser.add_argument(
+        '--unzip',
+        '-uz',
+        help='Unzip all downloaded files, one by one. NOTE: You can\'t combine' \
+             '--zip and --unzip options, it will throw error.',
+        action='store_true'
+    )
+
+    # Async process
+    parser.add_argument(
+        '--async',
+        help='Run zippyshare-downloader in asynchronous process',
+        action='store_true',
+        dest='async_process'
+    )
+
+    # Fast Download
+    parser.add_argument(
+        '--fast',
+        help='Enable Fast Download (Only available with --async option).',
+        action='store_true',
+    )
+
+    args = parser.parse_args()
+    urls = args.__dict__['ZIPPYSHARE_URL or FILE']
+    args.urls = urls
+    return args
+
+def setup_logging(name_module, verbose=False):
+    log = logging.getLogger(name_module)
+    handler = logging.StreamHandler()
+    fmt = logging.Formatter('[%(levelname)s] %(message)s')
+    handler.setFormatter(fmt)
+    log.addHandler(handler)
+    if verbose:
+        log.setLevel(logging.DEBUG)
+    else:
+        log.setLevel(logging.INFO)
+    return log
+
+def build_kwargs(args, urls):
+    kwargs = {
+        'urls': urls,
+        'download': not args.no_download,
+        'unzip': args.unzip,
+        'zip': args.zip,
+        'progress_bar': not args.silent,
+        'replace': args.replace,
+        'folder': args.folder,
+        'filename': args.filename,
+        'async': args.async_process,
+        'fast': args.fast
+    }
+    return kwargs
