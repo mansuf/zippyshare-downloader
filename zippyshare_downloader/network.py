89893d2bb56490bea3da5201b5dcc541023f912c
Rahman Yusuf <danipart4@gmail.com>
2022-01-02 12:55:51 +0700

    update network.py

--- zippyshare_downloader/network.py
+++ zippyshare_downloader/network.py
@@ -1,22 +1,19 @@
 import requests
 import aiohttp
 
-class ProxyObject:
-    def __init__(self, proxy=None):
-        self.proxy = None
-
-_proxy = ProxyObject()
+__all__ = (
+    'Net', 'set_proxy', 'clear_proxy'
+)
 
 # Modified requests session class with __del__ handler
 # so the session will be closed properly
 class requestsProxiedSession(requests.Session):
+    def __init__(self, trust_env=True) -> None:
+        super().__init__()
+        self.trust_env = trust_env
+
     def __del__(self):
         self.close()
-
-# requests session with trust_env disabled
-# this will disable proxies from enviroments
-_requests_session = requestsProxiedSession()
-_requests_session.trust_env = False
 
 # Because aiohttp doesn't support proxy from session
 # we need to subclass it to proxy each requests without
@@ -36,46 +33,74 @@ class aiohttpProxiedSession(aiohttp.ClientSession):
         kwargs.update(proxy=self.proxy)
         return await super()._request(*args, **kwargs)
 
-# Set to None to prevent RuntimeError
-_aiohttp_session = None # type: aiohttpProxiedSession
+class NetworkObject:
+    def __init__(self) -> None:
+        self._proxy = None
+        self._aiohttp = None # type: aiohttpProxiedSession
 
-def get_proxy():
-    """Return configured HTTP/SOCKS proxy"""
-    return _proxy.proxy
+        # This will be disable proxy from environtments
+        self._requests = requestsProxiedSession(trust_env=False)
 
-def is_proxied():
-    """Check if aiohttp/requests are configured to connect with proxy"""
-    return _proxy.proxy is not None
+    @property
+    def proxy(self):
+        """Return HTTP/SOCKS proxy, return ``None`` if not configured"""
+        return self._proxy
 
-def _create_aiohttp_session():
-    if _aiohttp_session is None:
-        # This is the ridiculous way to modify global variables
-        global_vars = globals()
-        global_vars['_aiohttp_session'] = aiohttpProxiedSession(proxy=None, trust_env=False)
+    @proxy.setter
+    def proxy(self, proxy):
+        if proxy is None:
+            self.clear_proxy()
+        self.set_proxy(proxy)
+
+    def is_proxied(self):
+        """Return ``True`` if requests/aiohttp from :class:`NetworkObject`
+        are configured using proxy.
+        """
+        return self.proxy is not None
+
+    def set_proxy(self, proxy):
+        """Setup HTTP/SOCKS proxy for aiohttp/requests"""
+        self._proxy = proxy
+        pr = {
+            'http': proxy,
+            'https': proxy
+        }
+        self._requests.proxies.update(pr)
+        self.aiohttp.set_proxy(proxy)
+
+    def clear_proxy(self):
+        """Remove all proxy from aiohttp/requests"""
+        self._proxy = None
+        self._requests.proxies.clear()
+        self.aiohttp.remove_proxy()
+
+    @property
+    def aiohttp(self):
+        """Return proxied aiohttp (if configured)"""
+        self._create_aiohttp()
+        return self._aiohttp
+
+    @property
+    def requests(self):
+        """Return proxied requests (if configured)"""
+        return self._requests
+
+    def _create_aiohttp(self):
+        if self._aiohttp is None:
+            self._aiohttp = aiohttpProxiedSession(self.proxy)
+
+Net = NetworkObject()
 
 def set_proxy(proxy):
-    """Set HTTP/SOCKS proxy to requests/aiohttp"""
-    _create_aiohttp_session()
-    pr = {
-        'http': proxy,
-        'https': proxy
-    }
-    _requests_session.proxies.update(pr)
-    _aiohttp_session.set_proxy(proxy)
-    _proxy.proxy = proxy
+    """Setup HTTP/SOCKS proxy for aiohttp/requests
+    
+    This is shortcut for :meth:`NetworkObject.set_proxy`
+    """
+    Net.set_proxy(proxy)
 
-def remove_proxy():
-    """Remove proxy from requests/aiohttp"""
-    _create_aiohttp_session()
-    _requests_session.proxies.clear()
-    _aiohttp_session.remove_proxy()
-    _proxy.proxy = None
-
-def get_proxied_requests():
-    """Return proxied requests session"""
-    return _requests_session
-
-def get_proxied_aiohttp():
-    """Return proxied aiohttp session"""
-    _create_aiohttp_session()
-    return _aiohttp_session
+def clear_proxy():
+    """Remove all proxy from aiohttp/requests
+    
+    This is shortcut for :meth:`NetworkObject.clear_proxy`
+    """
+    Net.clear_proxy()
