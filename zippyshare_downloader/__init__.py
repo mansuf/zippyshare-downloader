b470ac288980d35bd8ad10e44f48dd0d39a73efb
Rahman Yusuf <danipart4@gmail.com>
2022-01-03 13:38:45 +0700

    bump version v0.3.0

--- zippyshare_downloader/__init__.py
+++ zippyshare_downloader/__init__.py
@@ -1,7 +1,7 @@
 # zippyshare-downloader
 # __init__.py
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __description__ = "Download file from Zippyshare directly from python"
 __author__ = "mansuf"
 __license__ = "MIT"
